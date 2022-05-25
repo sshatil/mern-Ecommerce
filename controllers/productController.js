2781a40415e4d01e8e783bade2811246f6d478b2
sshatil <mutasimshatil@hotmail.com>
2022-05-26 05:32:16 +0800

    ecommerce REST API

---
+++ controllers/productController.js
@@ -0,0 +1,150 @@
+import asyncHandler from "express-async-handler";
+import Product from "../models/productModel.js";
+
+// @desc    Fetch all products
+// @route   GET /api/products
+// @access  Public
+const getProducts = asyncHandler(async (req, res) => {
+  const pageSize = 3;
+  const page = Number(req.query.pageNumber) || 1;
+  const name = req.query.name || "";
+  const category = req.query.category || "";
+  // const seller = req.query.seller || "";
+  const order = req.query.order || "";
+  const min =
+    req.query.min && Number(req.query.min) !== 0 ? Number(req.query.min) : 0;
+  const max =
+    req.query.max && Number(req.query.max) !== 0 ? Number(req.query.max) : 0;
+  const rating =
+    req.query.rating && Number(req.query.rating) !== 0
+      ? Number(req.query.rating)
+      : 0;
+
+  const nameFilter = name ? { name: { $regex: name, $options: "i" } } : {};
+  // const sellerFilter = seller ? { seller } : {};
+  const categoryFilter = category ? { category } : {};
+  const priceFilter = min && max ? { price: { $gte: min, $lte: max } } : {};
+  const ratingFilter = rating ? { rating: { $gte: rating } } : {};
+  const sortOrder =
+    order === "lowest"
+      ? { price: 1 }
+      : order === "highest"
+      ? { price: -1 }
+      : order === "toprated"
+      ? { rating: -1 }
+      : { _id: -1 };
+  const count = await Product.count({
+    // ...sellerFilter,
+    ...nameFilter,
+    ...categoryFilter,
+    ...priceFilter,
+    ...ratingFilter,
+  });
+  const products = await Product.find({
+    // ...sellerFilter,
+    ...nameFilter,
+    ...categoryFilter,
+    ...priceFilter,
+    ...ratingFilter,
+  })
+    // .populate("seller", "seller.name seller.logo")
+    .sort(sortOrder)
+    .skip(pageSize * (page - 1))
+    .limit(pageSize);
+  res.status(200).json({ products, page, pages: Math.ceil(count / pageSize) });
+});
+
+// Todo get products filter by brand
+
+const getProductCategory = asyncHandler(async (req, res) => {
+  const categories = await Product.find().distinct("category");
+  res.send(categories);
+});
+
+// @desc    Fetch single product
+// @route   GET /api/products/:id
+// @access  Public
+const getProductById = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+});
+
+// @desc    Delete a product
+// @route   DELETE /api/products/:id
+// @access  Private/Admin
+const deleteProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    await product.remove();
+    res.json({ message: "Product removed" });
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+});
+
+// @desc    Create new review
+// @route   POST /api/products/:id/reviews
+// @access  Private
+const createProductReview = asyncHandler(async (req, res) => {
+  const { rating, comment } = req.body;
+
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    const alreadyReviewed = product.reviews.find(
+      (r) => r.user.toString() === req.user._id.toString()
+    );
+
+    if (alreadyReviewed) {
+      res.status(400);
+      throw new Error("Product already reviewed");
+    }
+
+    const review = {
+      name: req.user.name,
+      rating: Number(rating),
+      comment,
+      user: req.user._id,
+    };
+
+    product.reviews.push(review);
+
+    product.numReviews = product.reviews.length;
+
+    product.rating =
+      product.reviews.reduce((acc, item) => item.rating + acc, 0) /
+      product.reviews.length;
+
+    await product.save();
+    res.status(201).json({ message: "Review added" });
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+});
+
+// @desc    Get top rated products
+// @route   GET /api/products/top
+// @access  Public
+const getTopProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({}).sort({ rating: -1 }).limit(3);
+
+  res.json(products);
+});
+
+export {
+  getProducts,
+  getProductCategory,
+  getProductById,
+  deleteProduct,
+  createProductReview,
+  getTopProducts,
+};
---
+++ routes/userRoutes.js
@@ -0,0 +1,28 @@
+import express from "express";
+const router = express.Router();
+import {
+  authUser,
+  registerUser,
+  getUserProfile,
+  updateUserProfile,
+  getUsers,
+  deleteUser,
+  getUserById,
+  updateUser,
+} from "../controllers/userController.js";
+import { protect, admin } from "../middleware/authMiddleware.js";
+
+router.route("/register").post(registerUser);
+router.post("/login", authUser);
+router.get("/", protect, admin, getUsers);
+router
+  .route("/profile")
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
+router
+  .route("/:id")
+  .delete(protect, admin, deleteUser)
+  .get(protect, admin, getUserById)
+  .put(protect, admin, updateUser);
+
+export default router;
