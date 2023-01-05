3a124c98f83f3682ff61744bffc7882c65619caa
sshatil <mutasimshatil@hotmail.com>
2023-01-05 17:31:09 +0800

    change server build path

--- server.js
+++ server.js
@@ -37,10 +37,10 @@ const __dirname = path.resolve();
 app.use("/public/uploads", express.static(__dirname + "/public/uploads"));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.use(express.static(path.join(__dirname, "/client/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
   );
 } else {
   app.get("/", (req, res) => {
