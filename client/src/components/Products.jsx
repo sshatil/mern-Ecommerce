e64bd576f60a932424bdc3bd2798601d047d9b32
sshatil <mutasimshatil@hotmail.com>
2023-01-05 13:08:36 +0800

    add pagination for product page

--- client/src/components/Products.jsx
+++ client/src/components/Products.jsx
@@ -1,6 +1,7 @@
 import { MenuAlt3Icon } from "@heroicons/react/outline";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import ReactPaginate from "react-paginate";
 import { getProducts } from "../redux/actions/proudctActions";
 import SearchBar from "./SearchBar";
 import SingleProduct from "./SingleProduct";
@@ -20,6 +21,23 @@ const Products = ({ showSidebar, setShowSidebar }) => {
   // useEffect(() => {
   //   setName("");
   // }, [loading]);
+
+  // pagination
+  const [itemOffset, setItemOffset] = useState(0);
+  const [currentItems, setCurrentItems] = useState(null);
+  const [pageCount, setPageCount] = useState(0);
+  const itemsPerPage = 6;
+
+  useEffect(() => {
+    const endOffset = itemOffset + itemsPerPage;
+    setCurrentItems(products.slice(itemOffset, endOffset));
+    setPageCount(Math.ceil(products.length / itemsPerPage));
+  }, [itemOffset, itemsPerPage, products]);
+  // Invoke when user click to request another page.
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * itemsPerPage) % products.length;
+    setItemOffset(newOffset);
+  };
   return (
     <div className="w-full overflow-hidden">
       {/* sidebar btn */}
@@ -36,11 +54,27 @@ const Products = ({ showSidebar, setShowSidebar }) => {
           {/* <h2 className="">Products</h2> */}
 
           <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-            {products.map((product) => (
+            {currentItems?.map((product) => (
               <>
                 <SingleProduct product={product} key={product._id} />
               </>
             ))}
+          </div>
+          <div className="my-8">
+            <ReactPaginate
+              breakLabel="..."
+              nextLabel=">"
+              onPageChange={handlePageClick}
+              pageRangeDisplayed={3}
+              pageCount={pageCount}
+              previousLabel="<"
+              renderOnZeroPageCount={null}
+              containerClassName="pagination"
+              pageLinkClassName="page-num"
+              previousLinkClassName="page-arrow"
+              nextLinkClassName="page-arrow"
+              activeLinkClassName="active"
+            />
           </div>
         </div>
       </div>
